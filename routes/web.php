e7a590946a957917e883b9e6c517cd5177cf83ac
claraanjelitakd <claraanjelitakd@gmail.com>
2025-12-08 22:52:30 +0700

    Merge

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
